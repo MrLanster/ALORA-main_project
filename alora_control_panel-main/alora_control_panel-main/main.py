f5463d8d52941262aa0388ceb745c05d9a0ae84b
1blackghost <ashishjosephnew@gmail.com>
2024-11-01 22:40:12 +0530

    tests

--- alora_control_panel-main/alora_control_panel-main/main.py
+++ alora_control_panel-main/alora_control_panel-main/main.py
@@ -2,22 +2,20 @@ import cv2
 from flask import Flask, render_template, Response, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Change this to a random secret key for session management
+app.secret_key = 'your_secret_key'  
 
-# Initialize the webcam
 camera = cv2.VideoCapture(0)
 
 def generate_frames():
     while True:
-        success, frame = camera.read()  # Read the frame from the camera
+        success, frame = camera.read() 
         if not success:
             break
         else:
-            # Encode the frame in JPEG format
             _, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()  # Convert the frame to bytes
+            frame = buffer.tobytes()  
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # Send frame as response
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -25,13 +23,12 @@ def index():
         username = request.form['username']
         password = request.form['password']
     
-        # Check if the credentials are correct
         if username == 'aloraadmin' and password == 'alorapassword':
-            session['username'] = username  # Add session
-            return redirect(url_for('main'))  # Redirect to main page
+            session['username'] = username  
+            return redirect(url_for('main'))  
         else:
-            flash('Invalid username or password')  # Show error message
-            return redirect(url_for('index'))  # Redirect back to login
+            flash('Invalid username or password') 
+            return redirect(url_for('index'))  
 
     return render_template('login.html')
 @app.route("/about")
@@ -42,12 +39,12 @@ def about():
 def main():
     if 'username' in session:
         return render_template('main.html', username=session['username'])
-    return redirect(url_for('index'))  # Redirect to login if not authenticated
+    return redirect(url_for('index'))  
 
 @app.route('/logout')
 def logout():
-    session.pop('username', None)  # Remove the session
-    return redirect(url_for('index'))  # Redirect to login page
+    session.pop('username', None)  
+    return redirect(url_for('index'))  
 
 @app.route('/video_feed')
 def video_feed():
